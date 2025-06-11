8347e282aa1f282d0c9d6be3e95f074f2c29031b
Surghe <Milesiii.Irving@gmail.com>
2025-06-11 12:29:49 -0600

    added a Menu item schmea

---
+++ BackEnd/src/models/MenuItems.js
@@ -0,0 +1,9 @@
+const mongoose = ('mongoose');
+
+const menuItemSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: String,
+  price: { type: Number, required: true },
+});
+
+export default mongoose.model('MenuItem', menuItemSchema);
